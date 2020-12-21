61b2ec48d422b8ac3985da569a1d2376711a9efc
TrickyBestia <thewonderfulcookie@gmail.com>
2020-12-21 21:17:03 +0300

    Glamourified PyPlugin.Example

---
+++ PyPlugin.Example/header.py
@@ -0,0 +1,5 @@
+# PRIORITY 1000
+
+NAME = "PyPlugin.Example"
+AUTHOR = "TrickyBestia"
+VERSION = "1.0.0.0"
--- PyPlugin.Example/plugin.py
+++ PyPlugin.Example/plugin.py
@@ -5,23 +5,15 @@ from MEC import Timing
 from UnityEngine import Application
 from Exiled.API.Features import Player
 
-NAME = "PyPlugin.Example"
-AUTHOR = "TrickyBestia"
-VERSION = "1.0.0.0"
-
 coroutine = 0
 
 def messagesBroadcasterCoroutine():
-    i = 0
-    while i < Application.targetFrameRate * MESSAGE_ROUND_START_DELAY:
-        i += 1
-        yield Timing.WaitForOneFrame
+    for i in range(Application.targetFrameRate * MESSAGE_ROUND_START_DELAY):
+            yield Timing.WaitForOneFrame
     while True:
         for player in Player.List:
             player.Broadcast(MESSAGE_DURATION, MESSAGE)
-        i = 0
-        while i < Application.targetFrameRate * MESSAGE_COOLDOWN:
-            i += 1
+        for i in range(Application.targetFrameRate * MESSAGE_COOLDOWN):
             yield Timing.WaitForOneFrame
 
 def onRoundStarted():
