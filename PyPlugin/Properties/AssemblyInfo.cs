58d1c48a11a8c476d143a6798f31477f0853748f
TrickyBestia <trickybestia@gmail.com>
2021-02-07 12:03:20 +0300

    Update copyright

--- PyPlugin/Properties/AssemblyInfo.cs
+++ PyPlugin/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PyPlugin")]
 [assembly: AssemblyCompany("TrickyBestia")]
 [assembly: AssemblyProduct("PyPlugin")]
-[assembly: AssemblyCopyright("Copyright © TrickyBestia 2020")]
+[assembly: AssemblyCopyright("Copyright © TrickyBestia 2020 - 2021")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.1")]
